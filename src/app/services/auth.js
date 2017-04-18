007ec88162b61ed5129a677b3fd232eefce95d6e
Anton Savoskin <sairus2k@gmail.com>
2017-04-18 22:32:21 +0700

    Add login method for Auth service

--- src/app/services/auth.js
+++ src/app/services/auth.js
@@ -4,8 +4,14 @@ import bcrypt from 'bcryptjs';
 export function auth($log) {
   'ngInject';
 
+  let email = '';
+  let authenticated = false;
+
   return {
-    signup
+    signup,
+    login,
+    email,
+    authenticated
   };
 
   function signup(user) {
@@ -16,5 +22,22 @@ export function auth($log) {
     } catch (e) {
       $log(e);
     }
+  }
+
+  function login(user) {
+    let localUser;
+    try {
+      localUser = angular.fromJson(localStorage.getItem('register'));
+    } catch (e) {
+      $log(e);
+    }
+    if (user.email === localUser.email && bcrypt.compareSync(user.password, localUser.hash)) {
+      email = user.email;
+      authenticated = true;
+      return true;
+    }
+    email = '';
+    authenticated = false;
+    return false;
   }
 }
