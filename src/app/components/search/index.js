00574d4996008979bf5b09838fd191b5d0fb7376
Anton Savoskin <sairus2k@gmail.com>
2017-04-19 21:46:32 +0700

    Add product filters for color and in stock

--- src/app/components/search/index.js
+++ src/app/components/search/index.js
@@ -6,5 +6,7 @@ export const search = {
     'ngInject';
 
     this.items = Products.get();
+    this.colors = this.items.map(item => item.color);
+    this.colors = [...new Set(this.colors)]; // Filter unique values
   }
 };
