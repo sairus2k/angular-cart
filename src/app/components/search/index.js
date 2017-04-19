e402c9b733780799295806613c0181b68cd6c54a
Anton Savoskin <sairus2k@gmail.com>
2017-04-19 22:25:06 +0700

    Add product filters for price range

--- src/app/components/search/index.js
+++ src/app/components/search/index.js
@@ -8,5 +8,8 @@ export const search = {
     this.items = Products.get();
     this.colors = this.items.map(item => item.color);
     this.colors = [...new Set(this.colors)]; // Filter unique values
+    this.range = (field, min = Number.MIN_VALUE, max = Number.MAX_VALUE) => {
+      return item => min <= item[field] && item[field] <= max;
+    };
   }
 };
