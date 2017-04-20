145f687de28eabd8380e24fe01354895945f1bca
Anton Savoskin <sairus2k@gmail.com>
2017-04-20 13:37:24 +0700

    Check for null value for price filter

--- src/app/components/search/index.js
+++ src/app/components/search/index.js
@@ -1,3 +1,4 @@
+import angular from 'angular';
 import './search.css';
 
 export const search = {
@@ -8,7 +9,13 @@ export const search = {
     this.items = Products.get();
     this.colors = this.items.map(item => item.color);
     this.colors = [...new Set(this.colors)]; // Filter unique values
-    this.range = (field, min = Number.MIN_VALUE, max = Number.MAX_VALUE) => {
+    this.range = (field, min, max) => {
+      if (angular.isUndefined(min) || min === null) {
+        min = Number.MIN_VALUE;
+      }
+      if (angular.isUndefined(max) || max === null) {
+        max = Number.MAX_VALUE;
+      }
       return item => min <= item[field] && item[field] <= max;
     };
   }
